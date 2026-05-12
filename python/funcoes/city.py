dbfb7f94bd6013b7938d6651c124b669a9d9037c
joaopaulo <joaopsf936@gmail.com>
2026-05-11 21:10:54 -0300

    anotacoes e codigo python

--- python/funcoes/city.py
+++ python/funcoes/city.py
@@ -12,3 +12,32 @@ def make_album(artista_album, titulo_album, faixa_album=''):
      return album
 
 print (make_album ('linkin park', 'meteora'))
+
+def make_album(artista, titulo):
+    album = {
+        'artista': artista,
+        'titulo': titulo
+    }
+    return album
+
+
+while True:
+    print("\nDigite o nome do artista:")
+    print("Digite 'q' para sair.")
+
+    artista = input()
+
+    if artista == 'q':
+        break
+
+    print("Digite o título do álbum:")
+    titulo = input()
+
+    if titulo == 'q':
+        break
+
+    album = make_album(artista, titulo)
+
+    print(album)
+
+
